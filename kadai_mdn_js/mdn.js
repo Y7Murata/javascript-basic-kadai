1b1872f9ed04c38dc7594ad97a80f902e4afd5ff
Y7Murata <156630740+Y7Murata@users.noreply.github.com>
2024-03-01 12:52:21 +0900

    Update mdn.js修正１

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,9 +1,9 @@
 const date = new Date();
 
 const [month, day, year] = [
-  date.getMonth()+6,
-  date.getDate()+21,
-  date.getFullYear()-1,
+  date.getMonth()+1,
+  date.getDate(),
+  date.getFullYear(),
 ];
 
 console.log(year + '年' + month +'月' + day + '日');
